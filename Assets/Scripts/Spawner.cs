cdf64c3a0712ac0ded3f2cdc4f4d5dfddfcfe6cf
MoLeighJackson <moleighjackson@gmail.com>
2019-03-10 21:58:27 -0400

    added ui
    added scrolling parallax background and floor. added spawner.

---
+++ Assets/Scripts/Spawner.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Spawner : MonoBehaviour {
+
+	public GameObject[] prefabs;
+	public float delay = 2.0f;
+	public bool active = true;
+	public Vector2 delayRange = new Vector2(1, 2);
+
+	// Use this for initialization
+	void Start () {
+		
+		StartCoroutine (ObstacleGenerator ());
+	}
+
+	IEnumerator ObstacleGenerator(){
+
+		yield return new WaitForSeconds (delay);
+
+		if (active) {
+			var newTransform = transform;
+
+			Instantiate(prefabs[Random.Range(0, prefabs.Length)], newTransform.position, Quaternion.identity);
+		
+		}
+
+		StartCoroutine (ObstacleGenerator ());
+
+	}
+
+
+}
